bbee48b36ab9be6944744d850ce42242895e991e
MateuszAdamski <235823@edu.p.lodz.pl>
2022-05-01 00:54:38 +0200

    5 fix: utworzenie stalych

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,19 +5,23 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
+    public static final int COUNT_IN_MAX_VALUE = 15;
+    public static final int RHYMERS_MAX_VALUE = 3;
+    public static final int MAX_RAND_VALUE = 20;
+
     public static void main(String[] args) {
         final Rhymersfactory factory = new DefaultRhymersFactory();
 
         DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 1; i < COUNT_IN_MAX_VALUE; i++)
+            for (int j = 0; j < RHYMERS_MAX_VALUE; j++)
                 rhymers[j].countIn(i);
 
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+        for (int i = 1; i < COUNT_IN_MAX_VALUE; i++)
+            rhymers[RHYMERS_MAX_VALUE].countIn(rn.nextInt(MAX_RAND_VALUE));
 
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
@@ -26,7 +30,7 @@ class RhymersDemo {
         }
 
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+                + ((HanoiRhymer) rhymers[RHYMERS_MAX_VALUE]).reportRejected());
 
     }
 
