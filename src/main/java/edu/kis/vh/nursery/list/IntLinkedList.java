17f4c77bd83a53ec9b7681c1ba954becaeca2aef
Cfnatic7 <michalpasicki1@gmail.com>
2022-05-09 22:01:41 +0200

    13 - znalezienie i usunięcie niepublicznej klasy

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,42 @@ package edu.kis.vh.nursery.list;
 
 // Klasa implementuje listę dwukierunkową
 public class IntLinkedList {
+	class Node {
+
+		private int value;
+		private Node prev;
+		private Node next;
+
+		// konstruktor inicjujący węzeł z konkretną wartością
+		public Node(int i) {
+			this.value = i;
+		}
+
+		// getter dla wartości w węźle
+		public int getValue() {
+			return value;
+		}
+
+		// getter dla poprzedniego węzła
+		public Node getPrev() {
+			return prev;
+		}
+
+		// ustawia wskaźnik na poprzedni węzeł
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		// getter dla następnego węzła
+		public Node getNext() {
+			return next;
+		}
+
+		// ustawia wskaźnik na następny węzeł
+		public void setNext(Node next) {
+			this.next = next;
+		}
+	}
 
 	private static final int EMPTY_LIST = -1;
 	private Node last;
